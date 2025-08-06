8ed8ac63d71e209dff8a343b9cd74db6af9f3ede
chubiby <72568998+chubibyy@users.noreply.github.com>
2025-08-06 23:17:09 +0200

    test run fail

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-
-// index.js - Point d'entrée principal Expo
-import 'react-native-gesture-handler' // IMPORTANT: Doit être en premier pour React Navigation
 import { registerRootComponent } from 'expo'
 import App from './App'
 
-// Configuration globale des polyfills si nécessaire
-import './src/utils/polyfills' // Optionnel: pour des polyfills custom
-
-// Configuration des erreurs globales (développement)
-if (__DEV__) {
-  import('./src/utils/reactotron').then(() => console.log('Reactotron Configured'))
-}
-
-// registerRootComponent remplace AppRegistry.registerComponent
-// Compatible avec Expo Go ET les builds natifs
 registerRootComponent(App)
